a5e1b344cdc6a2e400a83ff1e4499bd5abf62141
Nikhil Gohil <nkgohil007@gmail.com>
2020-03-23 12:15:27 +0530

    doc personal info done

---
+++ app/src/main/java/thesevenitsolutions/com/docshub/doctorpersonalinfo.java
@@ -0,0 +1,16 @@
+package thesevenitsolutions.com.docshub;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class doctorpersonalinfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doctorpersonalinfo);
+
+    }
+}
